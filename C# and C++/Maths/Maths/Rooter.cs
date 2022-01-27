75d1fb0d3e138ee22a27aa036f144db0b42d2dc7
Alexander Berg <alexandr.berg92@gmail.com>
2022-01-28 02:20:35 +0300

    Added C# and C++ tests

---
+++ C# and C++/Maths/Maths/Rooter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maths
+{
+    public class Rooter
+    {
+        public Rooter()
+        {
+        }
+        // estimate the square root of a number
+        public double SquareRoot(double x)
+        {
+            double estimate = x;
+            double diff = x;
+            while (diff > estimate / 1000)
+            {
+                double previousEstimate = estimate;
+                estimate = (estimate + x / estimate) / 2.0;
+                diff = Math.Abs(previousEstimate - estimate);
+            }
+            return estimate;
+        }
+        // Old code
+        //estimate = estimate - (estimate * estimate - x) / (2 * estimate);
+        
+        
+    }
+}
---
+++ C# and C++/RooterLib/RooterLib/RooterLib.cpp
@@ -0,0 +1,42 @@
+﻿#include "pch.h"
+#include "RooterLib.h"
+#include <math.h>
+#include <stdexcept>
+
+// constructor
+CRooterLib::CRooterLib()
+{
+}
+// Find the square root of a number.
+double CRooterLib::SquareRoot(double v)
+{
+	return 0.0;
+}
+
+
+// Find the square root of a number.
+double CRooterLib::SquareRoot(double v)
+{
+	double result = v;
+	double diff = v;
+	while (diff > result / 1000)
+	{
+		double oldResult = result;
+		result = result - (result * result - v) / (2 * result);
+		diff = abs(oldResult - result);
+	}
+	return result;
+}
+
+double CRooterLib::SquareRoot(double v)
+{
+	//Validate the input parameter:
+	if (v < 0.0)
+	{
+		throw std::out_of_range("Can't do square roots of negatives");
+	}
+
+	// old code
+//result = result - (result*result - v)/(2*result);
+// new code
+//	result = (result + v / result) / 2.0;
